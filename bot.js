c5dbf8526f5cb005f219627501f5d61f7331886d
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-09-06 12:38:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,27 +31,28 @@ client.user.setGame(`By:iTzFanForMe|=help`,"http://twitch.tv/S-F")
 
 
 
-client.on('message',async msg => {
-  var p = "!";
-  if(msg.content.startsWith(p + "=user")) {
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
-    time.overwritePermissions(msg.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-  setInterval(() => {
-      var currentTime = new Date(),
-Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
-Dat = currentTime.getDate()
-      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
- },1000);
-  });
-  }
- 
+client.on('message', message => {
+    if (message.content.startsWith("$id")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Name Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true) 
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+            .addField("``Your Name``", `${message.author.username}`)
+            .addField('``your tag``', message.author.discriminator)
+            .addField('``Your id``', message.author.id)
+            .addField('``Bot``', message.author.bot)
+            .addField('``date of registration``', message.author.createdAt)
+    })
+}
 });
+
 
 
 
