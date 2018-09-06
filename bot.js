84ac05eadc43e29bfb9be10437ea16d369f7118e
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-09-06 15:32:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,11 +75,28 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '$ping') {
-    msg.channel.send('Pong!');
-  }
-});
+
+
+
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='$member')  
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)  
+      .setFooter(message.author.username, message.author.avatarURL) 
+      .setTitle('🌷| Members info') 
+      .addBlankField(true)
+      .addField('📗| Online',  
+      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)  
+      .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)  
+      .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`) 
+      .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`) 
+      .addField('➡| Server Members',`${message.guild.memberCount}`) 
+      message.channel.send(IzRo);
+    
+    });
+
+
 
 
 
