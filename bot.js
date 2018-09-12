f9afae576436c8e041c3b709e83e34d3e1eb336c
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-09-12 17:20:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,59 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith("$id")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('``INFO Name Bot`` ')
-            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
-            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('``servers``', [client.guilds.size], true) 
-            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
-            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
-            .addField("``Your Name``", `${message.author.username}`)
-            .addField('``your tag``', message.author.discriminator)
-            .addField('``Your id``', message.author.id)
-            .addField('``Bot``', message.author.bot)
-            .addField('``date of registration``', message.author.createdAt)
-    })
-}
-});
-
-
-
-
-
-client.on('message', message => {
-    if(message.content.includes('discord.gg')){
-                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? 🤔   **');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** No Invite Links :angry: !**`)
-    }
-}
-});
-
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
 
 
 
 
 
-client.login(process.env.BOT_TOKEN);
+
+client.login('token');
