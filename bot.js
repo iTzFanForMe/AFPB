9d09947e93948745a2cac46d533104ac6d1dc9a0
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2019-11-04 20:03:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,29 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'سلام عليكم') {
+    msg.reply('وعليكم سلام حياك');
   }
 });
+
+
+client.on("ready", function() {
+  var words = [`By LORDS Team`, "LORDS BOTS™", "?help"];
+  var l = -1;
+  var b = 0;
+  var mss = "4000";
+  setInterval(function() {
+    if (l == -1) {
+      b = 1;
+    }
+    if (l == words.length - 1) {
+       b = -1;
+    }
+    l = l + b;
+    client.user.setGame(words[l], `http://www.twitch.tv/${client.user.username}`);
+  
+  }, mss);
+});
+
 
 client.login(process.env.BOT_TOKEN);
